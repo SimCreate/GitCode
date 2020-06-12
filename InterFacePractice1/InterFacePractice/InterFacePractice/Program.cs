324b7f744f8764afd3e3637df0592b317e01588a
SimCreate <wdg1057@gmail.com>
2020-06-12 16:00:26 +0900

    Add Project : InterfacePractice1

---
+++ InterFacePractice1/InterFacePractice/InterFacePractice/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFacePractice
+{
+    // You cannot use any access modifier for any member of an interface.
+    // All the members by defaults are public members.
+    // Interface can only contain declaration but not implementations.
+
+    interface IPen
+    {
+        bool Open(); 
+        bool Close();
+        void Write(string text);
+
+    }
+
+    class Pen : IPen
+    {
+        private string PenColor;
+
+        private bool isOpen = false;
+
+        public Pen (string pColor)
+        {
+            PenColor = pColor;
+        }
+
+        bool IPen.Open()
+        {
+            isOpen = true;
+            return isOpen;
+        }
+
+        bool IPen.Close()
+        {
+            isOpen = false;
+            return isOpen;
+        }
+
+        void IPen.Write(string pText)
+        {
+            if (isOpen)
+            {
+                Console.WriteLine(pText);
+            }
+            else
+            {
+                Console.WriteLine(PenColor + "펜을 열어주세요.");
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPen pen = new Pen("빨간색");
+            pen.Open();
+            pen.Write("test");
+            pen.Close();
+            pen.Write("test");
+            
+            Console.ReadKey();
+        }
+    }
+}
